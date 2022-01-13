b40680523ad2fb6db87b30dfd9a3dea20960a364
Atique Shaikh <atiqueshaikh@forbesmarshall.com>
2022-01-13 22:17:26 +0530

    Individual helper function defined.

--- Core/Src/Application.c
+++ Core/Src/Application.c
@@ -2155,3 +2155,87 @@ void ParameterRelayAlarmProcess(void)
 	}
 	RelayToggle();
 }
+
+/*
+ * Function name :- Application_ReadAcid
+ * Task :- Runs the acid pump and measures the raw absorbance.
+ */
+void Application_ReadAcid(void)
+{
+	//Run the acid pump
+	PumpOperation(0x01);
+
+	//perform the ADC measurement action when the pump action is completed
+	if(!PUMPControlHandle_t.u8Flag_measurement)
+	{
+		//HoldingRegister_t.ModeCommand_t.CommonCommand = COD_Measure;
+		CODADCCapture(COD_Measure);
+	}
+}
+
+/*
+ * Function name :- Application_ReadSample
+ * Task :- Runs the acid pump and measures the raw absorbance.
+ */
+void Application_ReadSample(void)
+{
+	//Run the acid pump
+	PumpOperation(0x02);
+
+	//perform the ADC measurement action when the pump action is completed
+	if(!PUMPControlHandle_t.u8Flag_measurement)
+	{
+		//HoldingRegister_t.ModeCommand_t.CommonCommand = COD_Measure;
+		CODADCCapture(COD_Measure);
+	}
+}
+
+/*
+ * Function name :- Application_SetAsZero
+ * Task :- Overwrite the existing PD1(0) and PD2(0) by taking the current PD1 and PD2 values.
+ */
+void Application_SetAsZero(uint8_t parameter)
+{
+	if(parameter == 1) /*< Parameter = COD*/
+	{
+		//set the current PD1 and PD2 mean values to the PD1(0) and PD2(0)
+		COD_MeasurementValues_t.PD1_Zero = COD_MeasurementValues_t.PD1_New;
+		COD_MeasurementValues_t.PD2_Zero = COD_MeasurementValues_t.PD2_New;
+
+		//Calculate the COD RAW
+		float COD_RAW = HoldingRegister_t.ModeCommand_t.COD_SF *(log(COD_MeasurementValues_t.PD1_Zero/COD_MeasurementValues_t.PD1_New) - log(COD_MeasurementValues_t.PD2_Zero/COD_MeasurementValues_t.PD2_New));
+		//Publish the values to the MODBUS
+		InputRegister_t.PV_info.COD_RAW = COD_RAW;
+		InputRegister_t.PV_info.PD1_0 = COD_MeasurementValues_t.PD1_Zero;
+		InputRegister_t.PV_info.PD2_0 = COD_MeasurementValues_t.PD2_Zero;
+		//Reset the command
+		HoldingRegister_t.ModeCommand_t.CommonCommand = 0;
+		//Flag set as data not saved in the FRAM
+		AWAOperationStatus_t.AWADataSave_Calibration = 0x01;
+	}
+	else
+	{
+		//set the current PD1 and PD2 mean values to the PD1(0) and PD2(0)
+		//COD_MeasurementValues_t.PD1_Zero = COD_MeasurementValues_t.PD1_New;
+		TSS_MeasurementValues_t.PD2_Zero = TSS_MeasurementValues_t.PD2_New;
+
+		//Calculate the COD RAW
+		float TSS_RAW = HoldingRegister_t.ModeCommand_t.TSS_SF *(log(TSS_MeasurementValues_t.PD2_Zero/TSS_MeasurementValues_t.PD2_New));
+		//Publish the values to the MODBUS
+		InputRegister_t.PV_info.TSS_RAW = TSS_RAW;
+		InputRegister_t.PV_info.TSS_PD2_0 = TSS_MeasurementValues_t.PD2_Zero;
+		//Reset the command
+		HoldingRegister_t.ModeCommand_t.CommonCommand = 0;
+		//Flag set as data not saved in the FRAM
+		AWAOperationStatus_t.AWADataSave_Calibration = 0x01;
+	}
+}
+
+
+
+
+
+
+
+
+
