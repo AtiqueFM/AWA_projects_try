ae8a6e1f944bb1f46609bdc80fd5131befad0839
Atique Shiakh <atiqueshaikh@forbesmarshall.com>
2022-08-22 16:30:43 +0530

    HMI version change :- 4.0.0
    Scheduler Interlock (Major Version)

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -33,7 +33,7 @@ extern "C" {
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 /*SOftware Version for HMI and Firmware*/
-#define HMI_VER_MAJOR		3 /*<*/
+#define HMI_VER_MAJOR		4 /*<*/
 #define HMI_VER_MINOR		0 /*<*/
 #define HMI_VER_BUGFIX		0 /*<*/
 #define FW_VER_MAJOR		2 /*<*/
